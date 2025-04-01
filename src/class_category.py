323360faf3f6f5b33682985c00d0323d2c5ed1b8
Your Name <you@example.com>
2025-04-01 20:20:38 +0300

    init_14.2_Access_modes_1

--- src/class_category.py
+++ src/class_category.py
@@ -1,29 +1,31 @@
 from src.class_product import Product
-from typing import List  # Импортируем List для аннотации списка
 
 
 class Category:
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 products: List[Product]):
+    def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.__products: List[Product] = []
+        self.__products: list[Product] = []  # Явная аннотация типа
         Category.category_count += 1
 
         for product in products:
             self.add_product(product)
 
     def add_product(self, product: Product):
+        if not isinstance(product, Product):
+            raise TypeError(
+                "Можно добавлять только объекты Product или его "
+                "наследников"
+            )
+
         self.__products.append(product)
         Category.product_count += 1
 
     @property
-    def products(self):
+    def products(self) -> str:  # Добавлена аннотация возвращаемого типа
         return "\n".join(
             [f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт."
              for p in self.__products]
